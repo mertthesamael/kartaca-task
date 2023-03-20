5630bfa4106d2c08eefd7703b95d30700a279ddd
Merto <mertenercan@gmail.com>
2023-03-20 15:07:51 +0300

    -backend template  -added signup system via Firebase

--- frontend/app/api/hello/route.js
+++ frontend/app/api/hello/route.js
@@ -1,3 +1,29 @@
+import { NextResponse } from "next/server";
+import admin from "firebase-admin"
+import credentials from "./kartaca-auction-firebase-adminsdk-d4jfr-aa352b479f.json"
+
+
+//Initialize firebase-admin sdk
+if(!admin.credential){
+  admin.initializeApp({
+    credential:admin.credential.cert(credentials)
+  })
+}
+
 export async function GET(request) {
-  return new Response('Hello, Next.js!')
+  return NextResponse.json({"test":'data'})
+}
+
+export async function POST(request) {
+  const res = await request.json();
+
+    const userResponse = await admin.auth().createUser({
+      email:res.email,
+      password:res.password,
+      emailVerified:false,
+      disabled:false
+      
+    })
+    return NextResponse.json({ 'data':userResponse })
+  
 }
